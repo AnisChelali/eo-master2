dd603cf68607bec9b131563bc79896c649f1758f
Adnane-bgh <130249248+Adnane-bgh@users.noreply.github.com>
2024-04-26 11:09:15 -1200

    update calsses2colors

--- eo_master2/visualization/classes2colors.py
+++ eo_master2/visualization/classes2colors.py
@@ -6,12 +6,12 @@ import json
 image = Image.open("T31TCJ_Toulouse_Nord_Classe.tif")
 img_np = np.array(image)
 
-# Load nomenclature data
+# Load classes_labels data
 with open('../constants/classes_labels.json', 'r') as json_file:
     data = json.load(json_file)
     categories = data['Level2']
 
-# Create a dictionary lookup for pixel codes
+# Create a dictionary for pixel codes
 code_to_rgb = {cat['code']: tuple(cat['RGB color']) for cat in categories.values()}
 
 # Create an RGB image
