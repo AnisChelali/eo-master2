0f04224b5aaf326770dfba0ccb1b18cce3435c56
AnisChelali <death34bba@gmail.com>
2024-04-28 12:34:21 +0100

    'code_refactoring'

--- eo_master2/ml/svm_train_test.py
+++ eo_master2/ml/svm_train_test.py
@@ -1,37 +1,82 @@
+import os
+from typing import Union
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import numpy as np
-import pickle
+import pickle as pkl
 
-# Function to train and evaluate the SVM classifier
-def train_and_evaluate(input_file:str , output_folder):
-    # Load the data 
-    data = np.load(input_file, allow_pickle=True).item()
-    X_train, y_train = data['X_train'], data['y_train']
-    X_test, y_test = data['X_test'], data['y_test']
 
+def load_data(filename: str) -> list[np.ndarray]:
+    data = np.load(filename, allow_pickle=True).item()
+    X_train, y_train = data["X_train"], data["y_train"]
+    X_test, y_test = data["X_test"], data["y_test"]
+    return X_train, y_train, X_test, y_test
+
+
+# Function to train and evaluate the knn classifier
+def train_knn_classifier(
+    X_train: np.ndarray, y_train: np.ndarray, output_model_file: str = None
+) -> SVC:
     # Initialize and train
     svm_classifier = SVC(kernel='rbf')
     svm_classifier.fit(X_train, y_train)
 
-    with open(output_folder, 'wb') as file:
-        pickle.dump(svm_classifier, file)
+    if not output_model_file is None:
+        print(f"Saving model to: {output_model_file}...")
+        with open(output_model_file, "wb") as file:
+            pkl.dump(svm_classifier, file)
 
+    return svm_classifier
+
+
+def test_model(model: SVC, test_test: Union[list, np.ndarray]):
     # Predict and evaluate
-    y_pred = svm_classifier.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
+    y_pred = model.predict(test_test)
+
+    return y_pred
+
+
+def evaluate(predicted_labels, groud_truth_labels, output_folder) -> None:
+
+    accuracy = accuracy_score(predicted_labels, groud_truth_labels)
 
     return accuracy
 
-accuracy_scores = []
 
-# Loop over each fold and perform training and evaluation
-for fold in range(1, 6):
-    accuracy = train_and_evaluate(f'././data/train_test_fold_{fold}.npy', f'././results/split_{fold}/svm_model.pkl')
-    accuracy_scores.append(accuracy)
+if __name__ == "__main__":
 
-mean_accuracy = np.mean(accuracy_scores)
-std_accuracy = np.std(accuracy_scores)
+    input_data_folder = "data/"
+    output_folder = "results/"
 
-print(f"Mean Accuracy: {mean_accuracy}")
-print(f"Standard Deviation: {std_accuracy}")
+    accuracy_scores = []
+    # Loop over each fold and perform training and evaluation
+    for fold in range(1, 6):
+
+        split_output_folder = f"{output_folder}split_{fold}/"
+        os.makedirs(split_output_folder, exist_ok=True)
+
+        X_train, y_train, X_test, y_test = load_data(
+            filename=f"{input_data_folder}train_test_fold_{fold}.npy"
+        )
+
+        model = train_knn_classifier(
+            X_train=X_train,
+            y_train=y_train,
+            output_model_file=f"{split_output_folder}svm_model.pkl",
+        )
+
+        y_predicted = test_model(model=model, test_test=X_test)
+        accuracy = evaluate(
+            predicted_labels=y_predicted,
+            groud_truth_labels=y_test,
+            output_folder=split_output_folder,
+        )
+
+        accuracy_scores.append(accuracy)
+
+    mean_accuracy = np.mean(accuracy_scores)
+    std_accuracy = np.std(accuracy_scores)
+
+    print(f"Mean Accuracy: {mean_accuracy}")
+    print(f"Standard Deviation: {std_accuracy}")
+
